0563192b7562b61603648e13a323bc627bf39000
wearixastore@gmail.com <detective0114@outlook.com>
2026-09-05 18:29:11 +0500

    Deploy portfolio branch to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/portfoloi/',
+    base: '/portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
